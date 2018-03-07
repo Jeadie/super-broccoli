aeb4ebbdbcc0a73063cc60576db1eeedff99e6eb
s4435436 <s4435436@moss.labs.eait.uq.edu.au>
2018-03-07 21:36:17 +1000

    Finished header reading and image loading

--- ML/data_parser.cpp
+++ ML/data_parser.cpp
@@ -9,8 +9,49 @@ typedef struct number_image_pairs {
 	int count; 
 } number_image_pairs;
 
+int32_t bytes_to_int32_t (char *bytes) {
+	return ((int32_t) *(bytes + 3)) +
+				((int32_t)*(bytes + 2) << 8) +
+				((int32_t)*(bytes + 1) << 16) +
+				((int32_t)*(bytes + 0) << 24);
+}
+
+uint32_t bytes_to_uint32_t(char *bytes) { 
+	return ((uint32_t) *bytes) +
+				((uint32_t)*(bytes + 1) << 8) +
+				((uint32_t)*(bytes + 2) << 16) +
+				((uint32_t)*(bytes + 3) << 24);
+}
+
 gsl_matrix_uchar* Get_Number_Images(FILE *file) {
-	gsl_matrix_uchar* images = gsl_matrix_uchar_alloc(1,1); 
+	char m_number[4], b_count[4], b_rows[4], b_columns[4];
+
+	if(fread(m_number, 1, 4, file) != 4||
+			fread(b_count, 1, 4, file) != 4 ||
+			fread(b_rows, 1, 4, file) != 4||
+			fread(b_columns, 1, 4, file) !=4){ 
+		fprintf(stdout, "Error reading header."); 
+		return (gsl_matrix_uchar * ) 0; 
+	}
+	if(m_number[0] != 0 || m_number[1] != 0) { 
+		fprintf(stdout, "Invalid Header."); 
+		return (gsl_matrix_uchar * ) 0; 
+	}
+	
+	int32_t rows = bytes_to_int32_t (b_rows); 
+	int32_t count = 60000; //bytes_to_int32_t (b_count); 
+	int32_t columns = bytes_to_int32_t (b_columns); 
+	fprintf(stdout, "rows %d\n columns %d\n count %d\n", rows, columns, count); 
+	gsl_matrix_uchar * images; 
+	for (int32_t i = 0; i < count; i++) {
+		gsl_matrix_uchar* image = gsl_matrix_uchar_calloc(rows, columns); 
+		if (gsl_matrix_uchar_fread(file, image) != 0) {
+			fprintf(stdout, "Error reading image from file."); 
+			return (gsl_matrix_uchar* ) 0; 
+		}; 
+		fprintf(stdout, "%d\n", i); // gsl_matrix_uchar_fprintf(stdout, image, "%d"); 
+		images[i] = *image; 
+	}
 	return images; 
 };
 
@@ -19,7 +60,7 @@ gsl_vector_uchar* Get_Number_Labels(FILE *file) {
 	
 	int read = fread(label_header, 1, 8, file); 
 	if( read != 8) {
-		fprintf(stdout, "Can't read header bytes: %d.", read); 
+		fprintf(stdout, "Can't read header bytes from labels: %d.", read); 
 		return (gsl_vector_uchar*) 0; 
 	}
 	if( label_header[0] || label_header[1]) {
@@ -28,6 +69,7 @@ gsl_vector_uchar* Get_Number_Labels(FILE *file) {
 	}
 	char byte_type = label_header[2]; 
 	int vector_dimension = label_header[3]; 
+	//TODO: fix to parsing the 4 bytes 
 	int32_t  label_count = 60000; //((int32_t) label_header[4] << 24) |  ((int32_t) label_header[5] << 16) |  ((int32_t) label_header[6] << 8) |  ((int32_t) label_header[7]); 
 
 //	fprintf(stdout, "count: %u %u %u %u %u %u %u %u",  label_header[6], label_header[1], label_header[2], label_header[3], label_header[4], label_header[5], label_header[6], label_header[7]);
@@ -53,12 +95,13 @@ number_image_pairs* Get_Number_Images_From_Idx(char* image_path, char* label_pat
 	FILE* label_file = fopen(label_path, "r");	
 	if( image_file == 0 || label_file ==0) {
 		fprintf(stdout, "Failed to open image or label files"); 
+		return (number_image_pairs*) 0; 
 	}
 	number_image_pairs *pairs = (number_image_pairs*) malloc(sizeof(number_image_pairs));	 
 	pairs->images = Get_Number_Images(image_file); 
 	pairs->labels = Get_Number_Labels(label_file); 
 	pairs->count = 10; //pairs->labels->size;		
-	gsl_vector_uchar_fprintf(stdout, pairs->labels, "%d"); 
+	//gsl_vector_uchar_fprintf(stdout, pairs->labels, "%d"); 
 	if(pairs->images == 0 || pairs->labels == 0) {
 		//TODO: Properly free non complete inner data structures
 		return (number_image_pairs *) 0;
@@ -67,7 +110,5 @@ number_image_pairs* Get_Number_Images_From_Idx(char* image_path, char* label_pat
 	fclose(label_file); 
 	return pairs; 
 };
-
-
 
 
